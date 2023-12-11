cc859750ea4e8d95d983f1f1d9ba76113e5049f4
Peter Westerström <peter.westerstrom@eonreality.com>
2023-12-11 20:38:11 +0100

    Day 9 solutions

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn part1() {
+fn part1and2() {
     let input_content = fs::read_to_string("input.txt").unwrap();
     let histories = input_content.lines().map(|x| {
         x.split(" ")
@@ -8,6 +8,7 @@ fn part1() {
             .collect::<Vec<_>>()
     });
     let mut sum = 0;
+    let mut sum2 = 0;
     for history in histories {
         println!();
         let mut sequences = Vec::new();
@@ -15,17 +16,15 @@ fn part1() {
 
         loop {
             let sequence = sequences.last().unwrap();
-            let t = sequence.iter().fold( (Vec::new(), 0),
-                |(mut v, last), x| {
-                    let y = x - last;
-                    v.push(y);
-                    (v, *x)
-                });
+            let t = sequence.iter().fold((Vec::new(), 0), |(mut v, last), x| {
+                let y = x - last;
+                v.push(y);
+                (v, *x)
+            });
             let mut t2 = t.0;
             t2.remove(0);
-            println!("{:?}", t2);
 
-            let all_zero = t2.iter().all(|x| *x==0);
+            let all_zero = t2.iter().all(|x| *x == 0);
             sequences.push(t2);
             if all_zero {
                 break;
@@ -33,10 +32,16 @@ fn part1() {
         }
 
         sum += sequences.iter().fold(0, |acc, xs| acc + xs.last().unwrap());
+
+        sum2 += sequences
+            .iter()
+            .rev()
+            .fold(0, |acc, xs| xs.first().unwrap() - acc);
     }
-    println!("{sum}");
+    println!("part1: {sum}");
+    println!("part2: {sum2}");
 }
 
 fn main() {
-    part1();
+    part1and2();
 }
